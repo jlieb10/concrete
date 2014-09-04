b75cbfc52294f90f969eab41b1e67f01e33d98a6
Joshua T. Lieberman <joshuathomaslieberman@gmail.com>
2014-09-04 17:17:32 -0400

    Refactored

--- config.ru
+++ config.ru
@@ -1,3 +1,6 @@
 require File.join(File.dirname(__FILE__), 'app.rb')
-
+$CLIENT = Twitter::REST::Client.new do |config|
+    config.consumer_key = ENV["TWITTER_KEY"]
+    config.consumer_secret = ENV["TWITTER_SECRET"]
+end
 run PoemApp::App
--- lib/tweet.rb
+++ lib/tweet.rb
@@ -1,12 +1,7 @@
 class TweetClass
-  CLIENT = Twitter::REST::Client.new do |config|
-    config.consumer_key = "RJihjlphxnk1BGVUxESbOEpDq"# ENV["TWITTER_KEY"]
-    config.consumer_secret = "N9K1lkEEZE0nspZhMo3zaCGC8PpmCXrhFM3xTSFsKBetlhFn2I"#ENV["TWITTER_SECRET"]
-  end
-
   def self.search(words)
     sometweets = []
-    CLIENT.search(words).take(7).map do |tweet|
+    $CLIENT.search(words).take(7).map do |tweet|
       if tweet.text.length > 20
         tweet_text = tweet.text.gsub("RT", "").gsub(/\b(http|https).+\w/i, "").gsub(/([\s]\s+)/, " ").gsub(/\@.+\b/, "").strip
         sometweets << "<a href='#{tweet.uri}'>#{tweet_text}</a>"
